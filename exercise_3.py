6ae24f15aa56081e112827c7df8f79b84f44901e
Dilshod-01 <valiyevdilshod99@gmail.com>
2021-05-11 15:40:08 +0500

    exercise3

---
+++ exercise_3.py
@@ -0,0 +1,12 @@
+def wash_car(amount_paid):
+    if (amount_paid == 12):
+        print("wash with tri-color foam")
+        print("rince twice")
+        print("dry with large blow dryer")
+    if (amount_paid == 6):
+        print("wash with white foam")
+        print("rince once")
+        print("air dry")
+
+
+wash_car(6)
